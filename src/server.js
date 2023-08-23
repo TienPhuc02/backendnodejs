134362f1e97e4a2c79480238461c08bba0fef65e
Phuc Do Tien <108790360+TienPhuc02@users.noreply.github.com>
2023-08-23 11:04:59 +0700

    remplate view engine

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const express = require("express"); //commonjs
 // import express from "express"; //es modules
+const path = require("path");
 const app = express(); //app express
 const port = 8080; //port
+
+//config template engine
+app.set("views", path.join(__dirname, "views"));// khai bao noi luu tru file engine
+app.set("view engine", "ejs");
 
 //khai bao routes
 app.get("/", (req, res) => {
@@ -12,7 +17,8 @@ app.get("/abc", (req, res) => {
   res.send("check abc!");
 });
 app.get("/phuc", (req, res) => {
-  res.send("<h1>check phuc!</h1>");
+  // res.send("<h1>check phuc!</h1>");
+  res.render("sample.ejs"); // render file sample -> tao ra 1 view động
 });
 
 //chay server
