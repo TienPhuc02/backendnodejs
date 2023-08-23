7f033bb85778488bc02bb93b2646e23895a48fef
Phuc Do Tien <108790360+TienPhuc02@users.noreply.github.com>
2023-08-23 10:44:50 +0700

    more routes

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-const express = require("express");
-// import express from "express";
-const app = express();
-const port = 8080;
+const express = require("express"); //commonjs
+// import express from "express"; //es modules
+const app = express(); //app express
+const port = 8080; //port
 
+//khai bao routes
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World !");
+});
+//app.method(path,function)
+app.get("/abc", (req, res) => {
+  res.send("check abc!");
+});
+app.get("/phuc", (req, res) => {
+  res.send("<h1>check phuc!</h1>");
 });
 
+//chay server
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
