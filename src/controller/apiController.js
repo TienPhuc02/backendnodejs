21b1503689ac76b7e3f3cb8f0995c3778cc22999
Phuc Do Tien <108790360+TienPhuc02@users.noreply.github.com>
2023-08-25 16:31:50 +0700

    create user

--- src/controller/apiController.js
+++ src/controller/apiController.js
@@ -6,4 +6,17 @@ const getUsersApi = async (req, res) => {
     data: results,
   });
 };
-module.exports = { getUsersApi };
+const postCreateUserApi = async (req, res) => {
+  let data = req.body;
+  let { email, name, city } = data;
+  let results = await User.create({
+    email: email,
+    name: name,
+    city: city,
+  });
+  return res.status(201).json({
+    errorCode: 0,
+    data: results,
+  });
+};
+module.exports = { getUsersApi, postCreateUserApi };
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const { getUsersApi } = require("../controller/apiController");
+const {
+  getUsersApi,
+  postCreateUserApi,
+} = require("../controller/apiController");
 const routerAPI = express.Router();
 routerAPI.get("/", (req, res) => {
   res.send("hello world with api");
 });
 routerAPI.get("/users", getUsersApi);
-
+routerAPI.post("/users", postCreateUserApi);
 module.exports = routerAPI;
