ad189baf0e32b1cf350399b39cecf0ea79cbf87a
Phuc Do Tien <108790360+TienPhuc02@users.noreply.github.com>
2023-08-26 08:20:21 +0700

    customer model update

--- src/model/Customer.js
+++ src/model/Customer.js
@@ -1,22 +1,25 @@
 const mongoose = require("mongoose");
-const customerSchema = new mongoose.Schema({
-  name: { type: String, require: true, unique: true, lowercase: true },
-  address: { type: String, require: true, unique: true, lowercase: true },
-  phone: { type: Number, min: [10, "at least 10 numbers"], max: 12 },
-  email: {
-    type: String,
-    trim: true,
-    lowercase: true,
-    unique: true,
-    require: [true, "Email address is  required!"],
-    match: [
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      "Please fill a valid email address",
-    ],
+const customerSchema = new mongoose.Schema(
+  {
+    name: { type: String, require: true, unique: true, lowercase: true },
+    address: { type: String, require: true, unique: true, lowercase: true },
+    phone: { type: Number, min: [10, "at least 10 numbers"], max: 12 },
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      unique: true,
+      require: [true, "Email address is  required!"],
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    image: { type: String, require: true, unique: true, lowercase: true },
+    description: { type: String, require: true, unique: true, lowercase: true },
   },
-  image: { type: String, require: true, unique: true, lowercase: true },
-  description: { type: String, require: true, unique: true, lowercase: true },
-});
+  { timestamps: true } //createAt,updatedAt
+);
 
 const Customers = mongoose.model("customer", customerSchema);
 module.exports = Customers;
