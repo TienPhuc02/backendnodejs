5d292d4f0e89aadc8ccfde81caf9c2d0680a73f5
Phuc Do Tien <108790360+TienPhuc02@users.noreply.github.com>
2023-08-26 08:18:55 +0700

    models customers

--- src/model/Users.js
+++ src/model/Users.js
@@ -9,16 +9,6 @@ const User = mongoose.model("users", UserSchema);
 
 module.exports = User;
 
-//SQL -> ORM (sequelize)
-//backend ->ORM -> generate raw query -> database
-//NoSQL -> ODM (mongoose)
-//backend->ODM(->generate raw query->database)
-//select * from user => find({}) ->[] find all
-
-//findOne({id:"123"})
-//findOne({name:"123"})
-
-//finById("123")->{}
 
 
 
