667ae75deb543ad65958d0a747de69edc0a7cd06
Phuc Do Tien <108790360+TienPhuc02@users.noreply.github.com>
2023-08-28 16:55:59 +0700

    create mongoDB driver

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require("express"); //commonjs
 require("dotenv").config();
+const { MongoClient } = require("mongodb");
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
 const apiUserRoutes = require("./routes/apiUser");
@@ -8,7 +9,6 @@ const mysql = require("mysql2");
 const connection = require("./config/database");
 const fileUpload = require("express-fileupload");
 const app = express(); //app express
-
 const port = process.env.PORT || 8080; //port
 const hostname = process.env.HOST_NAME || "localhost";
 
@@ -30,7 +30,20 @@ app.use("/v1/api/", apiCustomerRoutes);
 //test  connection
 (async () => {
   try {
-    await connection();
+    //using mongoose
+    // await connection();
+
+    //using mongodb driver
+    // Connection URL
+    const url = process.env.DB_HOST_WITH_DRIVER;
+    const client = new MongoClient(url);
+
+    // Database Name
+    const dbName = process.env.DB_NAME;
+    await client.connect();
+    console.log("Connected successfully to server");
+    const db = client.db(dbName);
+    const collection = db.collection("documents");
     app.listen(port, hostname, () => {
       console.log(`Backend app listening on port ${port}`);
     });
